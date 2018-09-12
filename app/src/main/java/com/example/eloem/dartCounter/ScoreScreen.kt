d2cc5d39fb696ee06477424796e985769822afac
ole-thoeb <43213977+ole-thoeb@users.noreply.github.com>
2018-09-12 20:02:13 +0200

    cleaned imports

--- app/src/main/java/com/example/eloem/dartCounter/ScoreScreen.kt
+++ app/src/main/java/com/example/eloem/dartCounter/ScoreScreen.kt
@@ -1,14 +1,11 @@
 package com.example.eloem.dartCounter
 
-import android.app.Activity
 import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
-
 import android.os.Bundle
 import android.support.v4.app.*
 import android.support.v4.view.ViewPager
 import android.util.SparseArray
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
